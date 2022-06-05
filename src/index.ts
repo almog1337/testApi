ecdfc7ea364ee5105a74b2111c7e9835452b4285
almog gueta <almog.gueta1741@gmail.com>
2022-06-05 21:01:07 +0300

    Change output string

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello almog')
+  res.send('Hello almog change works')
 })
 
 app.listen(port, () => {
