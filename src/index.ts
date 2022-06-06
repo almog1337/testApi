cd43b4ffb7f2c91be2ebb84faf9543e6924cf9fe
almog gueta <almog.gueta1741@gmail.com>
2022-06-06 13:53:31 +0300

    restore

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,11 @@
 const express = require('express');
 import { Request, Response } from "express";
-import { connectToDatabase } from "./database";
-import dotenv from "dotenv";
-import { addUser, deleteUser, getAll } from "./userService";
-
-dotenv.config();
 const app = express();
 const port = 3000;
 
-
 app.get('/', async (req: Request, res: Response) => {
-  res.send(await getAll());
+  res.send('hello hunters');
 });
-
-app.post('/:username', async (req: Request, res: Response) => {
-  if(! (req.params.username)) {
-    console.log("username undefined");
-    res.status(400).send();
-    return;
-  }
-
-  await addUser(req.params.username);
-  res.send();
-});
-
-app.delete('/:username', async (req: Request, res: Response) => {
-  if(! (req.params.username)) {
-    console.log("username undefined");
-    res.status(400).send();
-    return;
-  }
-
-  await deleteUser(req.params.username);
-  res.send();
-});
-
-connectToDatabase();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
