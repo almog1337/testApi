5d599a440765cc0e774606c2f65384aaf406acd5
almog gueta <almog.gueta1741@gmail.com>
2022-06-06 14:16:50 +0300

    noam

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', async (req: Request, res: Response) => {
-  res.send('hello hunters');
+  res.send('hello noam');
 });
 
 app.listen(port, () => {
